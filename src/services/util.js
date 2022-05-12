3732af9cfcd628c8c72c7f59662f9749e6b8c114
seunggwongit config --global user.email tjtmdrnjs1@gmail.com <tjtmdrnjs1@gmail.com>
2022-05-12 11:24:59 +0900

    modify : 영문소문자 => 영문으로 변경

--- src/services/util.js
+++ src/services/util.js
@@ -601,7 +601,9 @@ export function containPasswordCheck(str) {
     (?=.*[A-Z])       // should contain at least one upper case
     [a-zA-Z0-9]{8,}   // should contain at least 8 from the mentioned characters
   */
-  return str.match(/^(?=.*[a-z])(?=.*\d)(?=.*[@$!%*#?&])[a-z\d@$!%*#?&]{8,}$/);
+  return str.match(
+    /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+  );
 }
 
 export function convertKorPhoneFormat(str) {
