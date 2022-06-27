535d7eabea3d41a899dba88d4628c83e682f4bd0
kimkyutack <ekfekfdl4565@naver.com>
2022-06-27 15:18:12 +0900

    apply session time

--- src/screens/feed-book-image/FeedBookFeed.js
+++ src/screens/feed-book-image/FeedBookFeed.js
@@ -42,6 +42,8 @@ import {
 import { getFeedUser, getFeedAll } from '../../redux/book/BookActions';
 import { FeedBookFeedItem } from './FeedBookFeedItem';
 import { useIsFocused } from '@react-navigation/core';
+import { navigate } from '../../services/navigation';
+import { browsingTime } from '../../redux/session/SessionAction';
 
 export default function FeedBookFeed({ route, navigation }) {
   const user = useSelector(s => s.user);
@@ -52,6 +54,7 @@ export default function FeedBookFeed({ route, navigation }) {
   const dispatch = useDispatch();
   const listRef = useRef();
   const isFocused = useIsFocused();
+  const [sessionTime, setSessionTime] = useState('000000');
 
   const limit = 24;
   const [likeLoading, setLikeLoading] = useState(false);
@@ -100,6 +103,65 @@ export default function FeedBookFeed({ route, navigation }) {
       dispatch(getFeedAll(allPage + 1, limit, time, user.member_idx));
     }
   };
+
+  let hour = 0, minute = 0, second = -1;
+
+  function timeCount() {
+
+
+    let dsp_hour, dsp_minute, dsp_second;
+
+    second++;
+
+    if (minute == 60) {
+      hour++;
+      minute = 0;
+    }
+    if (second == 60) {
+      minute++;
+      second = 0;
+    }
+
+    if (hour < 10)
+      dsp_hour = '0' + hour;
+    else
+      dsp_hour = hour;
+
+    if (minute < 10)
+      dsp_minute = '0' + minute;
+    else
+      dsp_minute = minute;
+
+    if (second < 10)
+      dsp_second = '0' + second;
+    else
+      dsp_second = second;
+
+
+    let date_state = dsp_hour + dsp_minute + dsp_second;
+
+
+    setSessionTime(date_state);
+  };
+
+//page 로그 찍는 로직
+  useEffect(() => {
+    if (isFocused) {
+      var timer = setInterval(() => { timeCount() }, 1000);
+    }
+
+    if (!isFocused) {
+      if (sessionTime !== '000000') {
+
+        dispatch(browsingTime('피드북(전체페이지)', sessionTime));
+      }
+    }
+    return () => {
+      clearInterval(timer);
+      setSessionTime('000000');
+    }
+  }, [isFocused]);
+
 
   useEffect(() => {
     let mount = true;
